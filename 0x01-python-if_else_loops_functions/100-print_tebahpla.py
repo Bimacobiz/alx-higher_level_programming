c0e76ec7af1ce192324b65d295051ca479da5706
Bimacobiz <collinsmahigi@gmail.com>
2023-08-09 19:15:32 -0700

    task 100

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 100-print_tebahpla.py
+
+""""Prints the ASCII alphabet, in reverse order, 
+making sure to alternate lowercase and uppercase."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
