3faddea6c372d729ba1b8c9ccc1a096b95c02562
Bimacobiz <collinsmahigi@gmail.com>
2023-08-29 15:29:10 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class called Square."""
+
+
+class Square:
+    """Representation of a square."""
+
+    def __init__(self, size):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square created.
+        """
+        self.__size = size
