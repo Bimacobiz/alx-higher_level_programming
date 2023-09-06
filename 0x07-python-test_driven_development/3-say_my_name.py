d76dfc113d62b33ccf2f3ae3ce08af32fe2766f2
Bimacobiz <collinsmahigi@gmail.com>
2023-09-05 19:09:19 -0700

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 3-say_my_name.py
+"""Defines a name-printing function."""
+
+# The script starts with the shebang line to specify the Python interpreter.
+
+def say_my_name(first_name, last_name=""):
+    """Print a name.
+
+    Args:
+        first_name (str): The name that will be printed first.
+        last_name (str): The name that will be printed last.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+
+    # The function say_my_name takes two arguments: first_name and last_name.
+    # The last_name argument is optional and defaults to an empty string.
+
+    # Check if first_name is a string. If not, raise a TypeError.
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    # Check if last_name is a string. If not, raise a TypeError.
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    # If both first_name and last_name are strings, print the formatted name.
+    print("My name is {} {}".format(first_name, last_name))
+
