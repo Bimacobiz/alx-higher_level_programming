2d1233f70869d60f5a9c0edac48daf5c19b05098
Bimacobiz <collinsmahigi@gmail.com>
2023-09-08 17:20:53 -0700

    changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-"""Defines a function that will add an integer."""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """Return the result of a + b's integer addition.
+    """Return the integer addition of a and b.
 
-    Prior to addition, float arguments are typecast to ints.
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: In the event that either an or b is not an
-        integer or a float.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
-    
-# Test cases
-    try:
-        result = add_integer(3, 5)
-        print(result)  # This should print 8
-
-        result = add_integer(3.5, 5.2)
-        print(result)  # This should print 8
-
-        result = add_integer(3)
-        print(result)  # This should print 101 (3 + 98)
-    except TypeError as e:
-        print("Error:", e)
