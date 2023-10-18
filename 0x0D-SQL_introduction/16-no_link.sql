d0940dcba6ca89010d6e3c6c351bc60ad81c0a8b
Bimacobiz <collinsmahigi@gmail.com>
2023-10-17 19:11:20 -0700

    task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- It will list all the records of the table second_table having a name value in my MySQL server.
+-- The records will be ordered in descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
